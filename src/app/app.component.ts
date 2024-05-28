0b6d63c3a13788eb1755bdc3b922b1837c9d2d08
Lianne Reyes <123354523+livnne@users.noreply.github.com>
2024-05-28 21:52:43 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { NgIf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+export type EditorType = 'name' | 'profile';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <h1>Reactive Forms</h1>
+
+    <nav>
+      <button type="button" (click)="toggleEditor('name')">Name Editor</button>
+      <button type="button" (click)="toggleEditor('profile')">
+        Profile Editor
+      </button>
+    </nav>
+
+    <app-name-editor *ngIf="showNameEditor"></app-name-editor>
+
+    <app-profile-editor *ngIf="showProfileEditor"></app-profile-editor>
+  `,
+  styles: [
+    `
+      nav button {
+        padding: 1rem;
+        font-size: inherit;
+      }
+    `,
+  ],
+  imports: [
+    NameEditorComponent,
+    ProfileEditorComponent,
+    NgIf,
+    ReactiveFormsModule,
+  ],
+})
+export class AppComponent {
+  editor: EditorType = 'name';
+
+  get showNameEditor() {
+    return this.editor === 'name';
+  }
+
+  get showProfileEditor() {
+    return this.editor === 'profile';
+  }
+
+  toggleEditor(type: EditorType) {
+    this.editor = type;
+  }
+}
